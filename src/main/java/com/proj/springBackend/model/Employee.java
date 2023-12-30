2ff9f3e938bdb19602dfd19826a0150384e84616
Abul Fahad <abulfahad786@gmail.com>
2023-12-30 13:20:12 +0530

    C2 : Created Employee entity and EmployeeRepository

--- src/main/java/com/proj/springBackend/model/Employee.java
+++ src/main/java/com/proj/springBackend/model/Employee.java
@@ -1,15 +1,24 @@
 package com.proj.springBackend.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Entity
+@Table(name = "employee")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "firstName")
     private String firstName;
+    @Column(name = "lastName")
     private String lastName;
+    @Column(name = "email")
     private String email;
 }
